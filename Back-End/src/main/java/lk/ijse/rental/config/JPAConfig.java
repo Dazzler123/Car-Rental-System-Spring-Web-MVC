d6a8f73ab5dc4221ca42e6987c4c86bc58abae28
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 20:08:53 +0530

    Annotation added

--- Back-End/src/main/java/lk/ijse/rental/config/JPAConfig.java
+++ Back-End/src/main/java/lk/ijse/rental/config/JPAConfig.java
@@ -1,4 +1,12 @@
 package lk.ijse.rental.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+//@EnableJpaRepositories(basePackages = "lk.ijse.rental.dao")
 public class JPAConfig {
+
 }
--- Back-End/src/main/java/lk/ijse/rental/config/WebRootConfig.java
+++ Back-End/src/main/java/lk/ijse/rental/config/WebRootConfig.java
@@ -3,8 +3,10 @@ package lk.ijse.rental.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({JPAConfig.class})
 public class WebRootConfig {
     @Bean
     public ModelMapper modelMapper() {
