996cbec11e0ac41ed67887e8677f61af029c9c1a
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 14:15:11 +0530

    Configured MultipartResolver

--- Back-End/src/main/java/lk/ijse/Car_Rental_System/config/WebAppConfig.java
+++ Back-End/src/main/java/lk/ijse/Car_Rental_System/config/WebAppConfig.java
@@ -1,12 +1,27 @@
 package lk.ijse.Car_Rental_System.config;
 
 import lk.ijse.Car_Rental_System.controller.CustomerController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan(basePackageClasses = {CustomerController.class})
 @EnableWebMvc
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
+    //configure MultipartResolver
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
 }
