b56398644f7440a63ab29a388afcbefc5eb68c5f
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 20:14:43 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/advicer/AppWideExceptionHandler.java
+++ Back-End/src/main/java/lk/ijse/rental/advicer/AppWideExceptionHandler.java
@@ -1,4 +1,18 @@
 package lk.ijse.rental.advicer;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
 public class AppWideExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseUtil handlerException(RuntimeException e) {
+        return new ResponseUtil("500",e.getMessage(),null);
+    }
 }
