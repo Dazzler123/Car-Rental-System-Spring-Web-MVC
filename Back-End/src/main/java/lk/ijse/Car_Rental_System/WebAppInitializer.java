0deaca67c33e657b73d60a85c107ebec255fdefe
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 13:28:52 +0530

    Content updated

--- Back-End/src/main/java/lk/ijse/Car_Rental_System/WebAppInitializer.java
+++ Back-End/src/main/java/lk/ijse/Car_Rental_System/WebAppInitializer.java
@@ -1,7 +1,23 @@
 package lk.ijse.Car_Rental_System;
 
 
-public class WebAppInitializer{
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
 
