4933c062b8f555b05be356f6acdd629ca653f822
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 20:16:58 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/util/ResponseUtil.java
+++ Back-End/src/main/java/lk/ijse/rental/util/ResponseUtil.java
@@ -1,4 +1,16 @@
 package lk.ijse.rental.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class ResponseUtil {
+    private String code;
+    private String message;
+    private Object data;
 }
