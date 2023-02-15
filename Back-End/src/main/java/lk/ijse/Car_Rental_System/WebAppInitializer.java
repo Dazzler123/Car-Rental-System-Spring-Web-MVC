3f25036033c106be02c80789bfea4f6712e981ff
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 14:18:05 +0530

    Configured MultipartResolver

--- Back-End/src/main/java/lk/ijse/Car_Rental_System/WebAppInitializer.java
+++ Back-End/src/main/java/lk/ijse/Car_Rental_System/WebAppInitializer.java
@@ -4,6 +4,9 @@ import lk.ijse.Car_Rental_System.config.WebAppConfig;
 import lk.ijse.Car_Rental_System.config.WebRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -18,6 +21,11 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement(System.getProperty("java.io.tmpdir")));
     }
 }
 
