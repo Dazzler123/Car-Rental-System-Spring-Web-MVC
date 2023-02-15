c7f08b410edf9ab23157e4c27bca87700142f466
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 20:03:56 +0530

    Attributes and annotations added

--- Back-End/src/main/java/lk/ijse/Car_Rental_System/entity/Customer.java
+++ Back-End/src/main/java/lk/ijse/Car_Rental_System/entity/Customer.java
@@ -1,4 +1,27 @@
 package lk.ijse.Car_Rental_System.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Customer {
+    @Id
+    private String nic;
+    private String dl_no;
+    private String name;
+    private String address;
+    private String contact_no;
+    private String email;
+    private boolean nic_img;
+    private boolean dl_img;
+    private String gender;
 }
