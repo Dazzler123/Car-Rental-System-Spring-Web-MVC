f657e2a3320d94a13054a6b9f11bbddc0120eee8
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-15 19:58:58 +0530

    Introduced CustomerDTO

---
+++ Back-End/src/main/java/lk/ijse/Car_Rental_System/dto/CustomerDTO.java
@@ -0,0 +1,22 @@
+package lk.ijse.Car_Rental_System.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String nic;
+    private String dl_no;
+    private String name;
+    private String address;
+    private String contact_no;
+    private String email;
+    private boolean nic_img;
+    private boolean dl_img;
+    private String gender;
+}
