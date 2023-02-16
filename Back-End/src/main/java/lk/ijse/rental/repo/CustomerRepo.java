95ee4c7f43d360185e5472591e755e459ee70773
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 12:13:22 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/repo/CustomerRepo.java
+++ Back-End/src/main/java/lk/ijse/rental/repo/CustomerRepo.java
@@ -1,4 +1,7 @@
 package lk.ijse.rental.repo;
 
-public interface CustomerRepo {
+import lk.ijse.rental.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, String> {
 }
