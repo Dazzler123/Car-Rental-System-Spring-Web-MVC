f012869b1e4fc13ffa06eba8360d2c1a2c5421be
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 12:15:58 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/service/impl/CustomerServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,11 @@
 package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.CustomerDTO;
+import lk.ijse.rental.entity.Customer;
+import lk.ijse.rental.repo.CustomerRepo;
 import lk.ijse.rental.service.CustomerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -9,8 +13,20 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public void saveCustomer(CustomerDTO dto) {
-
+        if(repo.existsById(dto.getNic())) {
+            throw new RuntimeException("Customer Already Exists");
+        } else {
+            Customer customer = mapper.map(dto, Customer.class);
+            repo.save(customer);
+        }
     }
 }
