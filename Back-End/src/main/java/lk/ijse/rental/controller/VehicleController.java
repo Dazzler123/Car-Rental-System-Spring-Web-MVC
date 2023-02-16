f90cc30890ce7c4890a4a5a7f1e1ed59be3002e8
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 15:57:09 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/controller/VehicleController.java
+++ Back-End/src/main/java/lk/ijse/rental/controller/VehicleController.java
@@ -1,4 +1,17 @@
 package lk.ijse.rental.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vehicle")
+@CrossOrigin
 public class VehicleController {
+
+    @GetMapping
+    public void getAllVehicles() {
+
+    }
 }
