4d81636061914c3f0cfd96cd687ab08d177b456b
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 15:59:34 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/service/VehicleService.java
+++ Back-End/src/main/java/lk/ijse/rental/service/VehicleService.java
@@ -1,4 +1,9 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.VehicleDTO;
+
+import java.util.ArrayList;
+
 public interface VehicleService {
+    public ArrayList<VehicleDTO> loadAllVehicles();
 }
