97a9fe72042fb006960d381d58a54864dc21c5ab
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 15:46:41 +0530

    Attributed added

--- Back-End/src/main/java/lk/ijse/rental/dto/VehicleDTO.java
+++ Back-End/src/main/java/lk/ijse/rental/dto/VehicleDTO.java
@@ -1,4 +1,26 @@
 package lk.ijse.rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class VehicleDTO {
+    private String registration_no;
+    private String make;
+    private String model;
+    private int yom;
+    private String type; //general, premium, luxury
+    private String fuel_type;
+    private String color;
+    private String transmission;
+    private String mileage;
+    private int passenger_count;
+    private String description;  //paragraph on the card
+    private boolean reserved;  //is reserved or not
+    private boolean images;  // does it hv images uploaded or not
 }
