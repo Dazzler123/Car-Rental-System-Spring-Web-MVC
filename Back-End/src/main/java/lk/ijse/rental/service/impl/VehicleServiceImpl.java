b7a40cda19015ed0e2c858fe1c01904054369304
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 16:00:50 +0530

    Content Updated

--- Back-End/src/main/java/lk/ijse/rental/service/impl/VehicleServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/VehicleServiceImpl.java
@@ -1,4 +1,13 @@
 package lk.ijse.rental.service.impl;
 
-public class VehicleServiceImpl {
+import lk.ijse.rental.dto.VehicleDTO;
+import lk.ijse.rental.service.VehicleService;
+
+import java.util.ArrayList;
+
+public class VehicleServiceImpl implements VehicleService {
+    @Override
+    public ArrayList<VehicleDTO> loadAllVehicles() {
+        return null;
+    }
 }
