ee3ee20ca8da5c056c844c1a20a3560d0fde9ca8
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-16 16:06:54 +0530

    Get all used

--- Back-End/src/main/java/lk/ijse/rental/service/impl/VehicleServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/VehicleServiceImpl.java
@@ -1,13 +1,25 @@
 package lk.ijse.rental.service.impl;
 
 import lk.ijse.rental.dto.VehicleDTO;
+import lk.ijse.rental.repo.VehicleRepo;
 import lk.ijse.rental.service.VehicleService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 
 public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    VehicleRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
     @Override
     public ArrayList<VehicleDTO> loadAllVehicles() {
-        return null;
+        return mapper.map(repo.findAll(), new TypeToken<ArrayList<VehicleDTO>>() {
+        }.getType());
     }
 }
