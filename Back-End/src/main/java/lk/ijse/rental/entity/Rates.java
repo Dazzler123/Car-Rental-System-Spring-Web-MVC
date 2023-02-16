945de9c2cb37cb80c1ca0d2d62d3e851077e1482
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-17 00:17:41 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/entity/Rates.java
+++ Back-End/src/main/java/lk/ijse/rental/entity/Rates.java
@@ -1,4 +1,24 @@
 package lk.ijse.rental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Rates {
+    @Id
+    private String model;
+    private double daily_rate;
+    private double km_daily;
+    private double monthly_rate;
+    private double km_monthly;
+    private double extra_km_rate;
 }
