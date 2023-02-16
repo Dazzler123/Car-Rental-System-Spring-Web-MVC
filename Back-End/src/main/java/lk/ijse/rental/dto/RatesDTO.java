69456fd8a1b26b19ce6bf58899bcbc497820472d
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-17 00:16:25 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/dto/RatesDTO.java
+++ Back-End/src/main/java/lk/ijse/rental/dto/RatesDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class RatesDTO {
+    private String model;  //toyota corolla 2015
+    private double daily_rate;
+    private double km_daily;
+    private double monthly_rate;
+    private double km_monthly;
+    private double extra_km_rate;
 }
