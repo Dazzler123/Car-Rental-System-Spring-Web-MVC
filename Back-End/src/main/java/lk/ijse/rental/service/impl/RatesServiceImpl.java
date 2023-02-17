149b4657b19595781fcb0cd2ce9e7053dafd3b08
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-17 13:34:15 +0530

    Method implemented

--- Back-End/src/main/java/lk/ijse/rental/service/impl/RatesServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/RatesServiceImpl.java
@@ -1,4 +1,16 @@
 package lk.ijse.rental.service.impl;
 
-public class RatesServiceImpl {
+import lk.ijse.rental.dto.RatesDTO;
+import lk.ijse.rental.service.RatesService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class RatesServiceImpl implements RatesService {
+    @Override
+    public RatesDTO searchRateByModel() {
+        return null;
+    }
 }
