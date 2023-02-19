229391853fbe26e1139121cf5c3e3f7af3c4926c
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-19 21:59:15 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/entity/Rent.java
+++ Back-End/src/main/java/lk/ijse/rental/entity/Rent.java
@@ -1,4 +1,28 @@
 package lk.ijse.rental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Rent {
+    @Id
+    private String id;
+    private String date;
+    private String time;
+
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = Customer.class)
+    @JoinColumn(name = "nic")
+    private Customer nic;
+
+    @ManyToOne(cascade = CascadeType.ALL, targetEntity = Vehicle.class)
+    @JoinColumn(name = "registrationNo")
+    private String registrationNo;
 }
