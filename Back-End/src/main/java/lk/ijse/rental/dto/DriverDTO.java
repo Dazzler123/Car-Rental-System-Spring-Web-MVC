dade83ac0722215871ef395bcdad6d98b67d4ceb
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-22 12:43:08 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/dto/DriverDTO.java
+++ Back-End/src/main/java/lk/ijse/rental/dto/DriverDTO.java
@@ -1,4 +1,19 @@
 package lk.ijse.rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
+    private String nic;
+    private String dlNo;
+    private String name;
+    private String address;
+    private String contactNo;
+    private boolean occupied;   //is occupied or not (true or false)
 }
