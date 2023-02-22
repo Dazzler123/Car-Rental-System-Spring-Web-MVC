dcc251179fe9712218ab6b6d3060f8091c1f6bd9
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-22 15:32:13 +0530

    Method added

--- Back-End/src/main/java/lk/ijse/rental/service/impl/RentServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/RentServiceImpl.java
@@ -1,4 +1,18 @@
 package lk.ijse.rental.service.impl;
 
-public class RentServiceImpl {
+import lk.ijse.rental.dto.RentDTO;
+import lk.ijse.rental.service.RentService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RentServiceImpl implements RentService {
+    @Autowired
+    RentRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+    @Override
+    public void saveRent(RentDTO rent) {
+
+    }
 }
