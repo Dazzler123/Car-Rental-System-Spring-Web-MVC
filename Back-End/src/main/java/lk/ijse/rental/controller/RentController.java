fb44db701492af3734becd899d85e335610956c3
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-22 15:28:06 +0530

    Content updated

--- Back-End/src/main/java/lk/ijse/rental/controller/RentController.java
+++ Back-End/src/main/java/lk/ijse/rental/controller/RentController.java
@@ -1,4 +1,12 @@
 package lk.ijse.rental.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rent")
+@CrossOrigin
 public class RentController {
+
 }
