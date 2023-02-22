bb48fc95b64297ede74a30d19c25fa1ce8671d33
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-22 12:35:13 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/entity/Rent_Details.java
+++ Back-End/src/main/java/lk/ijse/rental/entity/Rent_Details.java
@@ -1,6 +1,29 @@
 package lk.ijse.rental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Rent_Details {
+    @Id
     private String id;
+    private String pickUpDate;
+    private String pickUpTime;
+    private boolean bankImg;
+    private double damageWaiver;  //saves 0 when placing the rent and after returning the car, if an damage was noticed
+    //that amount charged will be updated here
+    private String returnDate;
+    private String returnTime;
+    private String rentDuration;  //how many days rented
+
     private String driver;  //driver nic
 }
