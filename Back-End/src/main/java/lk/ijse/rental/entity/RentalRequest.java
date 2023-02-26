7cf82ff981e6827c27230d0851d072e749b17419
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 13:17:10 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/entity/RentalRequest.java
+++ Back-End/src/main/java/lk/ijse/rental/entity/RentalRequest.java
@@ -1,4 +1,31 @@
 package lk.ijse.rental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class RentalRequest {
+    @Id
+    private int requestId;
+    private String customerNic;
+    private String registrationNo;
+    private String driverNic;
+    private String date;
+    private String time;
+
+    private String pickUpDate;
+    private String pickUpTime;
+    private String bankImgKey;
+    private String returnDate;
+    private String returnTime;
+    private String rentDuration;
 }
