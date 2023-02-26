7fb37c8ba49adb3c02fc8ada70b9ca43ee7a513e
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-27 00:16:37 +0530

    Content updated

--- Back-End/src/main/java/lk/ijse/rental/service/MaintenanceService.java
+++ Back-End/src/main/java/lk/ijse/rental/service/MaintenanceService.java
@@ -1,4 +1,7 @@
 package lk.ijse.rental.service;
 
+import lk.ijse.rental.dto.MaintenanceDTO;
+
 public interface MaintenanceService {
+    public void recordVehicle(MaintenanceDTO dto);
 }
--- Back-End/src/main/java/lk/ijse/rental/service/impl/MaintenanceServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/MaintenanceServiceImpl.java
@@ -1,11 +1,28 @@
 package lk.ijse.rental.service.impl;
 
+import lk.ijse.rental.dto.MaintenanceDTO;
+import lk.ijse.rental.entity.Maintenance;
+import lk.ijse.rental.repo.MaintenanceRepo;
+import lk.ijse.rental.service.MaintenanceService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
-public class MaintenanceServiceImpl {
+public class MaintenanceServiceImpl implements MaintenanceService {
 
+    @Autowired
+    MaintenanceRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Override
+    public void recordVehicle(MaintenanceDTO dto) {
+        Maintenance vehicle = mapper.map(dto, Maintenance.class);
+        repo.save(vehicle);
+    }
 }
