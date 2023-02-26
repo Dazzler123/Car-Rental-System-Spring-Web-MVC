cdd117fda94e75dd4ed15008d840e686cee18972
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 22:03:20 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/entity/Defective_Vehicle.java
+++ Back-End/src/main/java/lk/ijse/rental/entity/Defective_Vehicle.java
@@ -1,4 +1,20 @@
 package lk.ijse.rental.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
 public class Defective_Vehicle {
+    @Id
+    String vehicleId;
+    String date; //date marked as defective
 }
