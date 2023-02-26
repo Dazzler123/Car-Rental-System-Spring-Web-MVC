edb52483a237529b086c8ff6e0b10d3c483f7d09
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 22:01:14 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/dto/DefectiveVehicleDTO.java
+++ Back-End/src/main/java/lk/ijse/rental/dto/DefectiveVehicleDTO.java
@@ -1,4 +1,15 @@
 package lk.ijse.rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class DefectiveVehicleDTO {
+    String vehicleId;
+    String date; //date marked as defective
 }
