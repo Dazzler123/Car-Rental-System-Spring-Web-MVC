f226b93e0546a61f26252ca59b034a3c39e8ac97
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 21:55:08 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/dto/MaintenanceDTO.java
+++ Back-End/src/main/java/lk/ijse/rental/dto/MaintenanceDTO.java
@@ -1,4 +1,14 @@
 package lk.ijse.rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class MaintenanceDTO {
+    String vehicleId;
 }
