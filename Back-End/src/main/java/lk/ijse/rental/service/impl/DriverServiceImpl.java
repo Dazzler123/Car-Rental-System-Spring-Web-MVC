b1df6a1d2f9cdaf6a62300b1de7f47b3cfb46737
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 18:08:05 +0530

    Content updated

--- Back-End/src/main/java/lk/ijse/rental/service/impl/DriverServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/DriverServiceImpl.java
@@ -1,4 +1,10 @@
 package lk.ijse.rental.service.impl;
 
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
 public class DriverServiceImpl {
 }
