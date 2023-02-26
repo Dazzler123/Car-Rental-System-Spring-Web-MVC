2ea1e4e58083cf372d6853764958a3e58300a4d1
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 18:04:49 +0530

    Content updated

--- Back-End/src/main/java/lk/ijse/rental/controller/DriverController.java
+++ Back-End/src/main/java/lk/ijse/rental/controller/DriverController.java
@@ -1,4 +1,10 @@
 package lk.ijse.rental.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/driver")
 public class DriverController {
+
 }
