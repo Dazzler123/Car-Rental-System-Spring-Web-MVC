2a43081140ea43fc794acc2de50f9e7a363038fb
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-26 13:14:14 +0530

    Attributes added

--- Back-End/src/main/java/lk/ijse/rental/dto/RentalRequestDTO.java
+++ Back-End/src/main/java/lk/ijse/rental/dto/RentalRequestDTO.java
@@ -1,4 +1,26 @@
 package lk.ijse.rental.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class RentalRequestDTO {
+    private int requestId;
+    private String customerNic; //customer id
+    private String registrationNo;  //vehicle id
+    private String driverNic;  //driver nic or save "SELF"
+    private String date;
+    private String time;
+
+    private String pickUpDate;
+    private String pickUpTime;
+    private String bankImgKey;   //image file key value
+    private String returnDate;
+    private String returnTime;
+    private String rentDuration;  //count of days of the rental period
 }
