5bf89f9ee07aa8b03df936b868f45ff6ada84f83
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-27 00:11:42 +0530

    Introduced MaintenanceController

---
+++ Back-End/src/main/java/lk/ijse/rental/controller/MaintenanceController.java
@@ -0,0 +1,13 @@
+package lk.ijse.rental.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/maintenance")
+@CrossOrigin
+public class MaintenanceController {
+
+
+}
