7dd67897b1117c35ade7f40f8c6ab45046db4786
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-27 00:12:48 +0530

    Introduced MaintenanceServiceImpl

---
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/MaintenanceServiceImpl.java
@@ -0,0 +1,11 @@
+package lk.ijse.rental.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class MaintenanceServiceImpl {
+
+}
