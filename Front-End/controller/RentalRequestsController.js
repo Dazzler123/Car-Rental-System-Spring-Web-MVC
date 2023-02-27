0f56f3b5bd3d218158d3e5ac7033de8928b034e8
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-27 18:30:44 +0530

    load all rental requests to the table added

--- Front-End/controller/RentalRequestsController.js
+++ Front-End/controller/RentalRequestsController.js
@@ -17,7 +17,18 @@ function loadAllRentalRequests() {
         method: "get",
         dataType: "json",
         success: function (resp) {
-            console.log(resp)
+            console.log(resp);
+            for (const c of resp.data) {
+                var row = '<tr><td>' + c.requestId + '</td><td>' + c.customerNic + '</td><td>' +
+                    c.registrationNo + '</td><td>' + c.driverNic + '</td><td>' + c.date + '</td><td>' + c.time +
+                    '</td><td>' +
+                    c.pickUpDate + '</td><td>' + c.pickUpTime + '</td><td>' + c.returnDate +
+                    '</td><td>' + c.returnTime + '</td><td>' + c.rentDuration + '</td></tr>';
+
+                //append to the table
+                $('#tblRequests').append(row);
+            }
+            // getRowDataToFields();
         },
         error: function (err) {
             alert(err.responseText.message);
