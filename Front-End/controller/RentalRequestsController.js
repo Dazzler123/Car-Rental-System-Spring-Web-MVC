bd5417dcde936e27c761bfc487e9672c4a97f140
Dazzler123 <dasinduhewagamage@gmail.com>
2023-02-27 18:44:08 +0530

    load all rental requests to the table added

--- Front-End/controller/RentalRequestsController.js
+++ Front-End/controller/RentalRequestsController.js
@@ -10,6 +10,9 @@ let baseURL = "http://localhost:8080/Back_End_war_exploded/";
 //load all rental requests to the table
 loadAllRentalRequests();
 
+//request id
+let requestID = "";
+
 
 function loadAllRentalRequests() {
     $.ajax({
@@ -28,10 +31,60 @@ function loadAllRentalRequests() {
                 //append to the table
                 $('#tblRequests').append(row);
             }
-            // getRowDataToFields();
+            getRowDataToFields();
         },
         error: function (err) {
             alert(err.responseText.message);
         }
     });
+}
+
+//get table row data
+function getRowDataToFields() {
+    $('#tblRequests > tr').click(function () {
+        var reqId = $(this).children(":eq(0)").text();
+        var customerId = $(this).children(":eq(1)").text();
+        var vehicleId = $(this).children(":eq(2)").text();
+        var driverId = $(this).children(":eq(3)").text();
+        var date = $(this).children(":eq(4)").text();
+        var time = $(this).children(":eq(5)").text();
+        var pickupDate = $(this).children(":eq(6)").text();
+        var pickupTime = $(this).children(":eq(7)").text();
+        var returnDate = $(this).children(":eq(8)").text();
+        var returnTime = $(this).children(":eq(9)").text();
+        var rentDuration = $(this).children(":eq(10)").text();
+
+        // set text
+        setTextFieldData(reqId, customerId, vehicleId, driverId, date, time, pickupDate, pickupTime, returnDate,
+            returnTime, rentDuration);
+
+        //set image
+        // loadNicDlImage();
+    });
+}
+
+
+function setTextFieldData(reqId, customerId, vehicleId, driverId, date, time, pickupDate, pickupTime, returnDate,
+                          returnTime, rentDuration) {
+    requestID = reqId;
+
+    //load customer & driver
+    loadCustomerDetails(customerId);
+    loadDriverDetails(driverId);
+
+    // $('#txtCustomerNIC').val(custNic);
+    // $('#txtCustomerName').val(custName);
+    // $('#txtCustomerAddress').val(custAddress);
+    // $('#txtCustomerDLNo').val(custDlNo);
+    // $('#txtCustomerEmail').val(custEmail);
+    // $('#txtCustomerContactNo').val(custContactNo);
+    // $('#cbxCustomerGender option:selected').text(custGender);
+}
+
+function loadCustomerDetails(id) {
+// $('#lblCustomerNIC').val(customerId);
+}
+
+function loadDriverDetails(id) {
+// $('#lblDriverNIC').val(driverId);
 }
