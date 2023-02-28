806d3e858cf7de10a1ccce8d51d5e0a725a9d89b
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-01 02:09:29 +0530

    Load driver details added

--- Front-End/controller/CalculateRentalFormController.js
+++ Front-End/controller/CalculateRentalFormController.js
@@ -36,9 +36,12 @@ $('#btnSearchRent').click(function () {
 
             //load customer details
             loadCustomerDetails(request.customerNic);
+
+            //load driver details
+            loadDriverDetails(request.driverNic);
         },
         error: function (err) {
-            alert("Incorrect Rent ID!");
+            alert("Incorrect Rent ID! Rent details not found.");
         }
     });
 });
@@ -60,4 +63,31 @@ function loadCustomerDetails(id) {
             alert(err.responseText.message);
         }
     });
+}
+
+
+function loadDriverDetails(id) {
+    if (id == "SELF") {
+        //set text
+        $('#lblDriverNIC').val("SELF");
+        $('#lblDriverDLNo').val("SELF");
+        $('#lblDriverName').val("SELF");
+    } else {
+        $.ajax({
+            url: baseURL + "driver/search?nic=" + id + "",
+            method: "get",
+            dataType: "json",
+            success: function (resp) {
+                console.log(resp);
+                var driver = resp.data;
+                //set data
+                $('#lblDriverNIC').val(driver.nic);
+                $('#lblDriverDLNo').val(driver.dlNo);
+                $('#lblDriverName').val(driver.name);
+            },
+            error: function (err) {
+                alert(err.responseText.message);
+            }
+        });
+    }
 }
