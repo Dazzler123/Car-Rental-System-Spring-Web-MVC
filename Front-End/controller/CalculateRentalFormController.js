cf1d114765007e7c89b223406f3448dbc8b4de22
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-01 02:02:12 +0530

    Load rent details added

---
+++ Front-End/controller/CalculateRentalFormController.js
@@ -0,0 +1,44 @@
+// backend url
+let baseURL = "http://localhost:8080/Back_End_war_exploded/";
+
+
+//ids
+let rentID;
+
+
+$('#btnSearchRent').click(function () {
+    //get request details
+    $.ajax({
+        url: baseURL + "rentalRequest/search?requestId=" + $('#txtRentID').val() + "",
+        method: "get",
+        dataType: "json",
+        success: function (resp) {
+            console.log(resp);
+            var request = resp.data;
+
+            rentID = request.requestId;
+
+            //set data
+            $('#lblPickUpDate').val(request.pickUpDate);
+            $('#lblPickUpTime').val(request.pickUpTime);
+            $('#lblReturnDate').val(request.returnDate);
+            $('#lblReturnTime').val(request.returnTime);
+            $('#lblRentDuration').val(request.rentDuration);
+
+            //load bank slip image
+            // Get data URL from localStorage
+            const url = localStorage.getItem(request.customerNic + request.driverNic + request.returnDate +
+                request.returnTime + request.registrationNo);
+
+            //set image
+            const img = document.querySelector("#bankSlipImage");
+            img.src = url;
+
+            //load customer details
+
+        },
+        error: function (err) {
+            alert("Incorrect Rent ID!");
+        }
+    });
+});
