bdb0f920511a1556e3307e3e1fd888a9c4f914c7
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-04 16:29:22 +0530

    Footer content updated

--- Front-End/controller/VehiclesDashboardController.js
+++ Front-End/controller/VehiclesDashboardController.js
@@ -1,22 +1,3 @@
-// // var titlearray =["css","js","python","java","android","jquery","ruby"];
-// // var descriptionarray =["css style","js program","python code","java objects","android program","jquery objects","ruby code"];
-// //
-// // var dynamic = document.querySelector('.container');
-// // for (var i = 0; i < titlearray.length; i++) {
-// //     var fetch = document.querySelector('.container').innerHTML;
-// //     dynamic.innerHTML = `<div id="cards${i}" class="boxes">
-// //       <div class="box-content">
-// //         <h2>${titlearray[i]}</h2>
-// //         <p>
-// //           ${descriptionarray[i]}
-// //         </p>
-// //         <a class="showmore" href="#">ReadMore</a>
-// //       </div>
-// //     </div>` + fetch ;
-// //     var bgimg = document.getElementById(`cards${i}`);
-// //     bgimg.style.backgroundImage = `url('img/${titlearray[i]}.jpg')`;
-// // }
-
 // loader
 $(window).on('load', function () {
     $("#loader_Container").fadeOut(700);
