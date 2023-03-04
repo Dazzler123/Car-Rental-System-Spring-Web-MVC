f5e5bbfe0b9013f37a67152810f21781c40342fc
Dazzler123 <dasinduhewagamage@gmail.com>
2023-03-04 20:16:06 +0530

    Content updated

--- Back-End/src/main/java/lk/ijse/rental/controller/RentRecordsController.java
+++ Back-End/src/main/java/lk/ijse/rental/controller/RentRecordsController.java
@@ -1,6 +1,11 @@
 package lk.ijse.rental.controller;
 
+import lk.ijse.rental.dto.RentRecordsDTO;
+import lk.ijse.rental.service.RentRecordsService;
+import lk.ijse.rental.util.ResponseUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rentRecords")
 @CrossOrigin
 public class RentRecordsController {
+
+    @Autowired
+    RentRecordsService service;
+
+    @PostMapping
+    public ResponseUtil saveRentDetails(RentRecordsDTO dto) {
+        service.saveRent(dto);
+        return new ResponseUtil("200","Rent details saved.", null);
+    }
 }
--- Back-End/src/main/java/lk/ijse/rental/service/impl/RentRecordsServiceImpl.java
+++ Back-End/src/main/java/lk/ijse/rental/service/impl/RentRecordsServiceImpl.java
@@ -1,10 +1,26 @@
 package lk.ijse.rental.service.impl;
 
+import lk.ijse.rental.dto.RentRecordsDTO;
+import lk.ijse.rental.repo.RentRecordsRepo;
+import lk.ijse.rental.service.RentRecordsService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
-public class RentRecordsServiceImpl {
+public class RentRecordsServiceImpl implements RentRecordsService {
+
+    @Autowired
+    RentRecordsRepo repo;
+
+    @Autowired
+    ModelMapper mapper;
+
+    @Override
+    public void saveRent(RentRecordsDTO dto) {
+
+    }
 }
